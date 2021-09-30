960e2b027547eed4b8d6b5d35588347e52c65972
Exonoesis <Blue_Flame_14@hotmail.com>
2021-09-29 18:40:50 -0600

    Replaced Template Code Base
    - Deleted Code from Hands on Rust Example
    - Added Code from Bevy ECS Tilemap Tiled Example

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,40 @@
-mod camera;
-mod components;
-mod map;
-mod map_builder;
-mod spawner;
-mod systems;
-mod turn_state;
+use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 
-mod prelude
-{
-    pub use bracket_lib::prelude::*;
-    pub use legion::systems::CommandBuffer;
-    pub use legion::world::SubWorld;
-    pub use legion::*;
-    pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT: i32 = 50;
-    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
-    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-    pub use crate::camera::*;
-    pub use crate::components::*;
-    pub use crate::map::*;
-    pub use crate::map_builder::*;
-    pub use crate::spawner::*;
-    pub use crate::systems::*;
-    pub use crate::turn_state::*;
+mod helpers;
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let handle: Handle<TiledMap> = asset_server.load("ortho-map.tmx");
+
+    let map_entity = commands.spawn().id();
+
+    commands.entity(map_entity)
+        .insert_bundle(TiledMapBundle {
+            tiled_map: handle,
+            map: Map::new(0u16, map_entity),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..Default::default()
+        });
 }
 
-use prelude::*;
-
-struct State
-{
-    ecs: World,
-    resources: Resources,
-    input_systems: Schedule,
-    player_systems: Schedule,
-    monster_systems: Schedule,
-}
-
-impl State
-{
-    fn new() -> Self
-    {
-        let mut ecs = World::default();
-        let mut resources = Resources::default();
-        let mut rng = RandomNumberGenerator::new();
-        let map_builder = MapBuilder::new(&mut rng);
-        spawn_player(&mut ecs, map_builder.player_start);
-        map_builder
-            .rooms
-            .iter()
-            .skip(1)
-            .map(Rect::center)
-            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
-        resources.insert(map_builder.map);
-        resources.insert(Camera::new(map_builder.player_start));
-        resources.insert(TurnState::AwaitingInput);
-        Self {
-            ecs,
-            resources,
-            input_systems: build_input_scheduler(),
-            player_systems: build_player_scheduler(),
-            monster_systems: build_monster_scheduler(),
-        }
-    }
-}
-
-impl GameState for State
-{
-    fn tick(&mut self, ctx: &mut BTerm)
-    {
-        ctx.set_active_console(0);
-        ctx.cls();
-        ctx.set_active_console(1);
-        ctx.cls();
-        self.resources.insert(ctx.key);
-        let current_state = *self.resources.get::<TurnState>().unwrap();
-        match current_state {
-            TurnState::AwaitingInput => self
-                .input_systems
-                .execute(&mut self.ecs, &mut self.resources),
-            TurnState::PlayerTurn => {
-                self.player_systems
-                    .execute(&mut self.ecs, &mut self.resources);
-            }
-            TurnState::MonsterTurn => self
-                .monster_systems
-                .execute(&mut self.ecs, &mut self.resources),
-        }
-        render_draw_buffer(ctx).expect("Render error");
-    }
-}
-
-fn main() -> BError
-{
-    let context = BTermBuilder::new()
-        .with_title("Dungeon Crawler")
-        .with_fps_cap(30.0)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
-        .with_tile_dimensions(32, 32)
-        .with_resource_path("resources/")
-        .with_font("dungeonfont.png", 32, 32)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .build()?;
-
-    main_loop(context, State::new())
+fn main() {
+      App::build()
+        .insert_resource(WindowDescriptor {
+            width: 1270.0,
+            height: 720.0,
+            title: String::from("Helping Hand"),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(TilemapPlugin)
+        .add_plugin(TiledMapPlugin)
+        .add_startup_system(startup.system())
+        .add_system(helpers::camera::movement.system())
+        .add_system(helpers::texture::set_texture_filters_to_nearest.system())
+        .run();
 }
