1e89d04823791b3a375889072412b75e39f2994f
Exonoesis <Blue_Flame_14@hotmail.com>
2022-04-23 03:47:58 -0600

    Merged Player Movement
    - Updated bevy version to 0.7
    - Replaced Eevee sprite with the one Sky made
    - Pulled in code from bevy_keyboard_test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
 use bevy::prelude::*;
+use mechanics::input::*;
+
+mod mechanics;
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Bundle)]
+struct PlayerBundle
+{
+    _p: Player,
+
+    #[bundle]
+    sprite: SpriteBundle,
+}
+
+fn spawn_player(mut commands: Commands, asset_spawner: Res<AssetServer>)
+{
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(PlayerBundle
+        {
+            _p: Player,
+            sprite: SpriteBundle
+            {
+                texture: asset_spawner.load("textures/characters/eevee_sky_v2.png"), 
+                ..default()
+            },
+        });
+}
 
 fn main()
 {
-    App::new().add_system(hello_world_system).run();
-}
-
-fn hello_world_system()
-{
-    println!("hello world");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(spawn_player)
+    .add_event::<Movement>()
+    .add_system(player_input)
+    .add_system(movement_logger)
+    .add_system(move_player)
+    .run();
 }
---
+++ src/mechanics/input.rs
@@ -0,0 +1,89 @@
+use bevy::prelude::*;
+use crate::Player;
+
+pub enum Movement {
+    Up,
+    Down,
+    Left,
+    Right,
+
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+}
+
+pub fn player_input(input: Res<Input<KeyCode>>, mut input_broadcast: EventWriter<Movement>) {
+    if input.pressed(KeyCode::W) && input.pressed(KeyCode::A) {
+        input_broadcast.send(Movement::UpLeft);
+    } else if input.pressed(KeyCode::W) && input.pressed(KeyCode::D) {
+        input_broadcast.send(Movement::UpRight);
+    } else if input.pressed(KeyCode::S) && input.pressed(KeyCode::A) {
+        input_broadcast.send(Movement::DownLeft);
+    } else if input.pressed(KeyCode::S) && input.pressed(KeyCode::D) {
+        input_broadcast.send(Movement::DownRight);
+    } else if input.pressed(KeyCode::W) {
+        input_broadcast.send(Movement::Up);
+    } else if input.pressed(KeyCode::S) {
+        input_broadcast.send(Movement::Down);
+    } else if input.pressed(KeyCode::A) {
+        input_broadcast.send(Movement::Left);
+    } else if input.pressed(KeyCode::D) {
+        input_broadcast.send(Movement::Right);
+    }
+}
+
+pub fn movement_logger(mut input_receiver: EventReader<Movement>) {
+    for movement_action in input_receiver.iter() {
+        let input_direction = match movement_action {
+            Movement::Up => "Up",
+            Movement::Down => "Down",
+            Movement::Left => "Left",
+            Movement::Right => "Right",
+            Movement::UpLeft => "UpLeft",
+            Movement::UpRight => "UpRight",
+            Movement::DownLeft => "DownLeft",
+            Movement::DownRight => "DownRight",
+        };
+
+        println!("Player input: {}", input_direction);
+    }
+}
+
+pub fn move_player(mut input_receiver: EventReader<Movement>, mut query: Query<(&mut Transform, &mut Sprite), With<Player>>) {
+    for movement_action in input_receiver.iter() {
+        let (mut player_transform, mut sprite) = query.single_mut();
+
+        let mut direction = Vec3::ZERO;
+        match movement_action {
+            Movement::Up => direction += Vec3::new(0.0, 3.0, 0.0),
+            Movement::Down => direction -= Vec3::new(0.0, 3.0, 0.0),
+            Movement::Left => {
+                direction -= Vec3::new(3.0, 0.0, 0.0);
+                sprite.flip_x = false;
+            },
+            Movement::Right => {
+                direction += Vec3::new(3.0, 0.0, 0.0);
+                sprite.flip_x = true;
+            },
+            Movement::UpLeft => {
+                direction += Vec3::new(-3.0, 3.0, 0.0);
+                sprite.flip_x = false;
+            },
+            Movement::UpRight => {
+                direction += Vec3::new(3.0, 3.0, 0.0);
+                sprite.flip_x = true;
+            },
+            Movement::DownLeft => {
+                direction -= Vec3::new(3.0, 3.0, 0.0);
+                sprite.flip_x = false;
+            },
+            Movement::DownRight => {
+                direction -= Vec3::new(-3.0, 3.0, 0.0);
+                sprite.flip_x = true;
+            }
+        }
+
+        player_transform.translation += direction;
+    }
+}
