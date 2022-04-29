3e659a100da904746f6b98c6538ccaf421d5d335
Exonoesis <Blue_Flame_14@hotmail.com>
2022-04-29 13:17:32 -0600

    Sprinkled delicious cinnamon sugar on the camera logic

--- src/mechanics/camera.rs
+++ src/mechanics/camera.rs
@@ -5,11 +5,10 @@ pub fn move_camera(
     player_query: Query<&Transform, (With<Player>, Changed<Transform>)>,
     mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>
 ){
-    if !player_query.is_empty()
-    {
-        let player_transform = player_query.single();
-        let mut camera_transform = camera_query.single_mut();
-
-        camera_transform.translation = player_transform.translation;
+    if let Ok(player_transform) = player_query.get_single() {
+        
+        if let Ok(mut camera_transform) = camera_query.get_single_mut() {
+            camera_transform.translation = player_transform.translation;
+        }        
     }
 }
