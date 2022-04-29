35a373a8e032f7f44cefb7c83c2b2987419847e5
Exonoesis <Blue_Flame_14@hotmail.com>
2022-04-29 12:27:42 -0600

    Camera Movement
    - Replaced Single Eevee Sprite w/ Multi-directional Set
    - Added Camera that exactly follows Player movement

---
+++ src/mechanics/camera.rs
@@ -0,0 +1,15 @@
+use crate::Player;
+use bevy::prelude::*;
+
+pub fn move_camera(
+    player_query: Query<&Transform, (With<Player>, Changed<Transform>)>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>
+){
+    if !player_query.is_empty()
+    {
+        let player_transform = player_query.single();
+        let mut camera_transform = camera_query.single_mut();
+
+        camera_transform.translation = player_transform.translation;
+    }
+}
