30c2928adde4008627f965a9419b837cc0c3d2b4
Exonoesis <Blue_Flame_14@hotmail.com>
2022-05-06 20:40:27 -0600

    Load Map
    - Added Map Loading Functionality
    - Added 64px Environment Tilesheet
    - Added New Test Map
    - Added 64x Eevee Spritesheet
    - Updated Name for Previous Snapshots
    - Added New Snapshot

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
 mod mechanics;
 
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 use mechanics::{camera::move_camera, input::*};
 
-#[derive(Component)]
+#[derive(Default, Component)]
 pub struct Player;
 
-#[derive(Bundle)]
+#[derive(Bundle, LdtkEntity)]
 struct PlayerBundle {
-    _p: Player,
-
+    #[sprite_bundle("textures/characters/EeveeSprites-l.png")]
     #[bundle]
     sprite: SpriteBundle,
+
+    player: Player,
 }
 
-fn spawn_player(mut commands: Commands, asset_spawner: Res<AssetServer>) {
+/// Loads the LDtk test map with a Camera into the game at the origin (0,0,0).
+fn spawn_map(mut commands: Commands, asset_spawner: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(PlayerBundle {
-        _p: Player,
-        sprite: SpriteBundle {
-            texture: asset_spawner.load("textures/characters/EeveeSprites-l.png"),
-            ..default()
-        },
+    commands.spawn_bundle(LdtkWorldBundle {
+        ldtk_handle: asset_spawner.load("maps/hh_test.ldtk"),
+        //transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
     });
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(spawn_player)
+        .add_plugin(LdtkPlugin)
+
+        .add_startup_system(spawn_map)
+        .insert_resource(LevelSelection::Index(0))
+
         .add_event::<Movement>()
         .add_system(player_input)
-        //.add_system(movement_logger)
         .add_system(move_player)
         .add_system(move_camera)
+
+        .register_ldtk_entity::<PlayerBundle>("Player")
         .run();
 }
