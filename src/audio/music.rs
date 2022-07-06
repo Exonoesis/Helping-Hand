f4aee9e32493b91d1853ba071b515b799ca8e71c
Exonoesis <Blue_Flame_14@hotmail.com>
2022-07-06 17:35:57 -0600

    Background Music Optimizations
    - Streamlined audio dependency
    - Negated if-statement in Music.rs

--- src/audio/music.rs
+++ src/audio/music.rs
@@ -3,14 +3,18 @@ use bevy_kira_audio::Audio;
 use bevy::prelude::*;
 
 pub fn play_level_music(asset_server: Res<AssetServer>, current_level_name: Res<LevelSelection>, audio: Res<Audio>) {
-    if current_level_name.is_changed() || current_level_name.is_added() {
-        let level_identifier = match &*current_level_name {
-            LevelSelection::Identifier(name) => name.clone(),
-            _ => panic!("Expected Level Identifier."),
-        };
-        
-        let audio_file_path = format!("audio/music/{}_overworld.wav", level_identifier);
+    let level_has_changed = current_level_name.is_changed() || current_level_name.is_added();
 
-        audio.play_looped(asset_server.load(&audio_file_path));
+    if !level_has_changed {
+        return;
     }
+
+    let level_identifier = match &*current_level_name {
+        LevelSelection::Identifier(name) => name.clone(),
+        _ => panic!("Expected Level Identifier."),
+    };
+        
+    let audio_file_path = format!("audio/music/{}_overworld.wav", level_identifier);
+
+    audio.play_looped(asset_server.load(&audio_file_path));
 }
