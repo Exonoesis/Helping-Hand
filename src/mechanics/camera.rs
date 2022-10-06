e5d4eeb2458ca6d5389ab54962818cf812a8dd05
Exonoesis <Blue_Flame_14@hotmail.com>
2022-10-05 21:49:37 -0600

    World Bound Camera Attempt 1
    - Set Camera to stay within the world bounds
    - Removed original move_camera function
    - Integrated camera following player functionality into new move_camera function [KNOWN ERROR]

--- src/mechanics/camera.rs
+++ src/mechanics/camera.rs
@@ -1,13 +1,38 @@
-use crate::entities::player::Player;
+use crate::{entities::player::Player, LevelDimensions};
 use bevy::prelude::*;
 
 pub fn move_camera(
+    level_dimension: Res<LevelDimensions>,
     player_query: Query<&Transform, (With<Player>, Changed<Transform>)>,
-    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+    mut camera_query: Query<(&mut Transform, &OrthographicProjection), (Changed<Transform>, With<Camera2d>, Without<Player>)>,
 ) {
-    if let Ok(player_transform) = player_query.get_single() {
-        if let Ok(mut camera_transform) = camera_query.get_single_mut() {
-            camera_transform.translation = player_transform.translation;
-        }
+    if camera_query.is_empty() {
+        return;
+    }
+
+    if level_dimension.height == 0 || level_dimension.width == 0 {
+        return;
+    }
+
+    let (mut camera_transform, camera_bounds) = camera_query.get_single_mut().unwrap();
+    let player_transform = player_query.get_single().unwrap();
+    
+    let camera_width = (camera_bounds.right - camera_bounds.left).abs() + 1.0;
+    let camera_height = (camera_bounds.top - camera_bounds.bottom).abs() + 1.0;
+
+    if camera_width > level_dimension.width as f32 {
+        camera_transform.translation.x = level_dimension.width as f32 / 2.0;
+        camera_transform.translation.y = player_transform.translation.y;
+    }
+    else {
+        camera_transform.translation.x = camera_transform.translation.x.clamp(camera_width / 2.0, level_dimension.width as f32 - (camera_width / 2.0));
+    }
+
+    if camera_height > level_dimension.height as f32 {
+        camera_transform.translation.y = level_dimension.height as f32 / 2.0;
+        camera_transform.translation.x = player_transform.translation.x;
+    }
+    else {
+        camera_transform.translation.y = camera_transform.translation.y.clamp(camera_height / 2.0, level_dimension.height as f32 - (camera_height / 2.0));
     }
 }
