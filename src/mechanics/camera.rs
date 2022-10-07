9736135045c17f6e232cb813d05be8e6f94284ff
Exonoesis <Blue_Flame_14@hotmail.com>
2022-10-07 08:22:38 -0600

    Camera Follow Player Resolved
    - Camera properly follows player again

--- src/mechanics/camera.rs
+++ src/mechanics/camera.rs
@@ -4,9 +4,13 @@ use bevy::prelude::*;
 pub fn move_camera(
     level_dimension: Res<LevelDimensions>,
     player_query: Query<&Transform, (With<Player>, Changed<Transform>)>,
-    mut camera_query: Query<(&mut Transform, &OrthographicProjection), (Changed<Transform>, With<Camera2d>, Without<Player>)>,
+    mut camera_query: Query<(&mut Transform, &OrthographicProjection), (With<Camera2d>, Without<Player>)>,
 ) {
     if camera_query.is_empty() {
+        return;
+    }
+
+    if player_query.is_empty() {
         return;
     }
 
@@ -22,17 +26,15 @@ pub fn move_camera(
 
     if camera_width > level_dimension.width as f32 {
         camera_transform.translation.x = level_dimension.width as f32 / 2.0;
-        camera_transform.translation.y = player_transform.translation.y;
     }
     else {
-        camera_transform.translation.x = camera_transform.translation.x.clamp(camera_width / 2.0, level_dimension.width as f32 - (camera_width / 2.0));
+        camera_transform.translation.x = player_transform.translation.x.clamp(camera_width / 2.0, level_dimension.width as f32 - (camera_width / 2.0));
     }
 
     if camera_height > level_dimension.height as f32 {
         camera_transform.translation.y = level_dimension.height as f32 / 2.0;
-        camera_transform.translation.x = player_transform.translation.x;
     }
     else {
-        camera_transform.translation.y = camera_transform.translation.y.clamp(camera_height / 2.0, level_dimension.height as f32 - (camera_height / 2.0));
+        camera_transform.translation.y = player_transform.translation.y.clamp(camera_height / 2.0, level_dimension.height as f32 - (camera_height / 2.0));
     }
 }
