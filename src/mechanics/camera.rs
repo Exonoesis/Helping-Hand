d9fdbd21f8f1b3be7cce6bbfc822334e9af1d23b
Exonoesis <Blue_Flame_14@hotmail.com>
2022-10-14 20:30:10 -0600

    Added Unit Tests
    - Made Unit Tests for camera bounding behavior

--- src/mechanics/camera.rs
+++ src/mechanics/camera.rs
@@ -38,3 +38,452 @@ pub fn move_camera(
         camera_transform.translation.y = player_transform.translation.y.clamp(camera_height / 2.0, level_dimension.height as f32 - (camera_height / 2.0));
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_LEVEL_WIDTH: usize = 500;
+    const TEST_LEVEL_HEIGHT: usize = 500;
+
+    const CAMERA_HEIGHT: usize = 100;
+    const CAMERA_WIDTH: usize = 100;
+
+    const CAMERA_MIDPOINT: usize = 50;
+
+    const TEST_LEVEL_WIDTH_IN_BOUNDS: f32 = 250.0;
+    const TEST_LEVEL_WIDTH_OUT_LBOUNDS: f32 = -500.0;
+    const TEST_LEVEL_WIDTH_OUT_RBOUNDS: f32 = 1000.0;
+
+    const TEST_LEVEL_HEIGHT_IN_BOUNDS: f32 = 250.0;
+    const TEST_LEVEL_HEIGHT_OUT_TBOUNDS: f32 = 1000.0;
+    const TEST_LEVEL_HEIGHT_OUT_BBOUNDS: f32 = -500.0;
+
+    fn setup_app_bounds_checking() -> App {
+        let mut app = App::new();
+
+        app.insert_resource(LevelDimensions {
+            width: TEST_LEVEL_WIDTH,
+            height: TEST_LEVEL_HEIGHT,
+        });
+
+        app.add_system(move_camera);
+
+        app
+    }
+
+    #[test]
+    fn within_bounds() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_IN_BOUNDS, 
+                    TEST_LEVEL_HEIGHT_IN_BOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = TEST_LEVEL_WIDTH_IN_BOUNDS;
+        let expected_transform_y = TEST_LEVEL_HEIGHT_IN_BOUNDS;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_left() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_OUT_LBOUNDS, 
+                    TEST_LEVEL_HEIGHT_IN_BOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = CAMERA_MIDPOINT as f32;
+        let expected_transform_y = TEST_LEVEL_HEIGHT_IN_BOUNDS;
+
+        let actual_transform = *camera_query.unwrap();
+
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_topleft() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_OUT_LBOUNDS, 
+                    TEST_LEVEL_HEIGHT_OUT_TBOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = CAMERA_MIDPOINT as f32;
+        let expected_transform_y = (TEST_LEVEL_HEIGHT - CAMERA_MIDPOINT) as f32;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_bottomleft() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_OUT_LBOUNDS, 
+                    TEST_LEVEL_HEIGHT_OUT_BBOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = CAMERA_MIDPOINT as f32;
+        let expected_transform_y = CAMERA_MIDPOINT as f32;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_right() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_OUT_RBOUNDS, 
+                    TEST_LEVEL_HEIGHT_IN_BOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = (TEST_LEVEL_WIDTH - CAMERA_MIDPOINT) as f32;
+        let expected_transform_y = TEST_LEVEL_HEIGHT_IN_BOUNDS;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_topright() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_OUT_RBOUNDS, 
+                    TEST_LEVEL_HEIGHT_OUT_TBOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = (TEST_LEVEL_WIDTH - CAMERA_MIDPOINT) as f32;
+        let expected_transform_y = (TEST_LEVEL_HEIGHT - CAMERA_MIDPOINT) as f32;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_bottomright() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_OUT_RBOUNDS, 
+                    TEST_LEVEL_HEIGHT_OUT_BBOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = (TEST_LEVEL_WIDTH - CAMERA_MIDPOINT) as f32;
+        let expected_transform_y = CAMERA_MIDPOINT as f32;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_top() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_IN_BOUNDS, 
+                    TEST_LEVEL_HEIGHT_OUT_TBOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = TEST_LEVEL_HEIGHT_IN_BOUNDS;
+        let expected_transform_y = (TEST_LEVEL_HEIGHT - CAMERA_MIDPOINT) as f32;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+
+    #[test]
+    fn out_of_bounds_bottom() {
+        let mut app = setup_app_bounds_checking();
+
+        let _player_id = app
+            .world
+            .spawn()
+            .insert_bundle((
+                Player,
+                Transform::from_xyz(
+                    TEST_LEVEL_WIDTH_IN_BOUNDS, 
+                    TEST_LEVEL_HEIGHT_OUT_BBOUNDS,
+                    0.0),
+            ))
+            .id();
+
+        let camera_id = app
+            .world
+            .spawn()
+            .insert_bundle(Camera2dBundle {
+                projection: OrthographicProjection { 
+                    left: (0.0), 
+                    right: ((CAMERA_WIDTH - 1) as f32), 
+                    bottom: (0.0), 
+                    top: ((CAMERA_HEIGHT - 1) as f32), 
+                    ..default()},
+                ..default()
+            })
+            .id();
+
+        app.update();
+
+        let camera_query = app.world.get::<Transform>(camera_id);
+        assert!(camera_query.is_some());
+
+        let expected_transform_x = TEST_LEVEL_WIDTH_IN_BOUNDS;
+        let expected_transform_y = CAMERA_MIDPOINT as f32;
+
+        let actual_transform = *camera_query.unwrap();
+        let actual_transform_x = actual_transform.translation.x;
+        let actual_transform_y = actual_transform.translation.y;
+
+        assert_eq!(expected_transform_x, actual_transform_x);
+        assert_eq!(expected_transform_y, actual_transform_y);
+    }
+}
