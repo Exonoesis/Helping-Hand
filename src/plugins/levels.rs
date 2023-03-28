863b69fbc5089a8b9473f27a20a5260e94336b7f
Exonoesis <Blue_Flame_14@hotmail.com>
2023-03-27 22:56:31 -0600

    Added States
    - Modified levels and playable_character plugins to utilize states when loading systems

--- src/plugins/levels.rs
+++ src/plugins/levels.rs
@@ -5,21 +5,29 @@ use bevy_kira_audio::AudioApp;
 use crate::{
     audio::music::{play_level_music, MusicChannel},
     mechanics::{camera::*, input::*},
-    visuals::map::*,
+    visuals::map::*, 
+    AppState,
 };
 
 pub struct LevelsPlugin;
 
 impl Plugin for LevelsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_map)
+        app
+            .add_system_set(
+                SystemSet::on_enter(AppState::InGame)
+                .with_system(spawn_map)
+            )
             .insert_resource(LevelSelection::Identifier("Level_0".to_string()))
             .init_resource::<LevelDimensions>()
-            .add_system(move_camera)
-            .add_system(player_input)
-            .add_system(play_level_music)
-            .add_system(update_level_dimensions)
-            .add_system(update_camera_on_resolution_change)
+            .add_system_set(
+                SystemSet::on_update(AppState::InGame)
+                .with_system(move_camera)
+                .with_system(player_input)
+                .with_system(play_level_music)
+                .with_system(update_level_dimensions)
+                .with_system(update_camera_on_resolution_change)
+            )
             .add_audio_channel::<MusicChannel>()
             .add_event::<Movement>();
     }
