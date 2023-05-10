ebd99a1c6d256d80900efe0e556bceb404495bdb
Exonoesis <Blue_Flame_14@hotmail.com>
2023-05-10 01:43:29 -0600

    PR Revisions 1
    - Deleted sneaky vscode settings file
    - Refactored main_menu_button code to be more concise given the current usage

--- src/mechanics/main_menu_buttons.rs
+++ src/mechanics/main_menu_buttons.rs
@@ -10,18 +10,13 @@ pub fn button_system(
     mut interaction_query: Query<(&Interaction, &MainMenuButtonTypes), (Changed<Interaction>, With<Button>)>
 ) {
     for (interaction, button_type) in &mut interaction_query {
-        match *interaction {
-            Interaction::Clicked => {
-                match button_type {
-                    MainMenuButtonTypes::Play => next_state.set(AppState::InGame),
-                    MainMenuButtonTypes::Quit => exit_event.send(AppExit)
-                }
-            }
-            Interaction::Hovered => {
-                //println! ("Hover");
-            }
-            Interaction::None => {
-            }
+        if *interaction != Interaction::Clicked {
+            continue;
+        }
+        
+        match button_type {
+            MainMenuButtonTypes::Play => next_state.set(AppState::InGame),
+            MainMenuButtonTypes::Quit => exit_event.send(AppExit)
         }
     }
 }
