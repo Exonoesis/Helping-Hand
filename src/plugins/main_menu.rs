ae10ed93ad86e4bab1eb1486864d0a2d93171970
Exonoesis <Blue_Flame_14@hotmail.com>
2023-06-04 01:18:01 -0600

    PR Suggested Optimizations
    - Added some safety precautions to ensure that systems are bound by state
    - Updated Cargo

--- src/plugins/main_menu.rs
+++ src/plugins/main_menu.rs
@@ -11,9 +11,9 @@ pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu))            )
-        .add_systems((load_background_image,load_button_image,load_text_font))      
-        .add_system(button_system)
+        .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+        .add_systems((load_background_image,load_button_image,load_text_font).in_set(OnUpdate(AppState::MainMenu)))
+        .add_system((button_system).in_set(OnUpdate(AppState::MainMenu)))
         .add_system(unload_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }
 }
