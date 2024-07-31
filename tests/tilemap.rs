d0a776cb868504d56e538ffb7d0ed343c6d50bc7
Exonoesis <Blue_Flame_14@hotmail.com>
2024-07-30 20:03:03 -0600

    Work on spawning map
    - Created Tile struct
    - Wrote spawn_map function

--- tests/tilemap.rs
+++ tests/tilemap.rs
@@ -3,11 +3,25 @@ use bevy::prelude::*;
 use cucumber::{given, then, when, World};
 
 use helping_hand::map::*;
+use tiled::Loader;
+use ldtk_json::LdtkJson;
+
+use std::fs;
 
 #[derive(Debug, Default, World)]
 #[world(init = Self::new)]
 struct GameWorld {
     pub app: App,
+}
+
+struct Tile {
+    tile_px_width: u32,
+    tile_px_height: u32,
+    px_x: u32,
+    px_y: u32,
+    px_z: u32,
+    //layer_number: u32,
+    //properties:
 }
 
 impl GameWorld {
@@ -20,23 +34,49 @@ impl GameWorld {
     }
 }
 
-fn spawn_map(mut commands: Commands) {}
+fn spawn_map() {
+    let mut loader = Loader::new();
+    let tiled_map = loader.load_tmx_map("test-assets/maps/test_map.tmx").unwrap();
+    
+    let tile_width = tiled_map.tile_width;
+    let tile_height = tiled_map.tile_height;
 
-#[given("a LDtk map called test_map.ldtk,")]
-fn verify_test_map_exists(world: &mut GameWorld) {
-    unimplemented!()
+    let map_width = tiled_map.width;
+    let map_height = tiled_map.height;
+
+    for y in 0..map_height {
+        for x in 0..map_width {
+            let tile = Tile {
+                tile_px_width: tile_width,
+                tile_px_height: tile_height,
+                px_x: x * tile_width,
+                px_y: y * tile_height,
+                px_z: 0,
+            };
+            spawn_tile(tile);
+        }
+    }
 }
 
-#[when("the LDtk map is loaded,")]
-fn load_test_map(world: &mut GameWorld) {
-    world.app.add_systems(Startup, spawn_map);
-    world.app.update();
+fn spawn_tile(tile: Tile){
+    //TO-DO
 }
 
-#[then("there are 4x4 (16) tiles loaded in a grid.")]
-fn verify_loaded_tile_amount(world: &mut GameWorld) {
-    unimplemented!()
-}
+// #[given("a LDtk map called test_map.ldtk,")]
+// fn verify_test_map_exists(world: &mut GameWorld) {
+//     unimplemented!()
+// }
+
+// #[when("the LDtk map is loaded,")]
+// fn load_test_map(world: &mut GameWorld) {
+//     world.app.add_systems(Startup, spawn_map);
+//     world.app.update();
+// }
+
+// #[then("there are 4x4 (16) tiles loaded in a grid.")]
+// fn verify_loaded_tile_amount(world: &mut GameWorld) {
+//     unimplemented!()
+// }
 
 // This runs before everything else, so you can setup things here.
 fn main() {
